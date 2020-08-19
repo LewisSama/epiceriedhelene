dc26fcc64286e83dd12da6bf41ce85c148a4631b
LewisSama <lou.lesage09@gmail.com>
2020-08-19 14:56:22 +0200

    added first skeleton of pages

---
+++ src/Controller/ConfirmationController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ConfirmationController extends AbstractController
+{
+    /**
+     * @Route("/validation")
+     * @return Response
+     */
+    public function index(){
+
+        return $this->render('confirmation/index.html.twig');
+
+    }
+}
